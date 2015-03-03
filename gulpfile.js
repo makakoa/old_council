c8d0bbb4e1cb9bc9b69d9e98d28a4124c959670f
makakoa <makakoa15@gmail.com>
2015-03-02 19:18:25 -0800

    added code check

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+'use strict';
+
 var gulp = require('gulp');
+var fs = require('fs');
 var jshint = require('gulp-jshint');
 var jsxcs = require('gulp-jsxcs');
 var clean = require('gulp-clean');
@@ -11,6 +14,25 @@ var paths = {
   front: ['app/**/*.{js,jsx}'],
   back: ['server/**/*.js']
 };
+
+//to read in json files
+var _jsonCfg = function(name) {
+  var raw = fs.readFileSync(name).toString();
+  return JSON.parse(raw.replace(/\/\/.*\n/g, ''));
+};
+
+gulp.task('jshint', function() {
+  return gulp
+    .src([].concat(paths.front, paths.back, 'gulpfile.js'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('jscs', function() {
+  return gulp
+    .src([].concat(paths.front, paths.back, 'gulpfile.js'))
+    .pipe(jsxcs(_jsonCfg('.jscsrc')));
+});
 
 gulp.task('clean', function() {
   return gulp
@@ -35,4 +57,4 @@ gulp.task('server', function() {
   });
 });
 
-gulp.task('default', ['clean', 'build:dev', 'server']);
+gulp.task('default', ['jshint', 'jscs', 'clean', 'build:dev', 'server']);
