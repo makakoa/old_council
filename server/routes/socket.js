c2e768364c82ed5de5d32aabd9515618dd85773c
Cameron <cameron@nomic.com>
2016-05-09 12:58:28 -0700

    add nano / couchdb

--- server/routes/socket.js
+++ server/routes/socket.js
@@ -1,124 +1,123 @@
 'use strict';
 
+var _ = require('lodash');
+
 var userCount = 0;
 var councilSize = 0;
-var open = {};
-var recent = [{}, {}, {}, {}, {}];
+var openQuestions = {};
+var recent = [];
 
-//auto populate for dev
-open = {
-  suchunique: {
-    start: Date.now() - 29000,
-    time: 30000,
-    prompt: 'What is life?',
-    options: [{option: 'a game', votes: 0}, {option: 'a mystery', votes: 0}, {option: 'the matrix', votes: 0}],
-    total: 0,
-    _id: 'suchunique'
-  },
-  suchunique2: {
-    start: Date.now() - 28000,
-    time: 30000,
-    prompt: 'How many fingers am I holding up?',
-    options: [{option: 'one', votes: 0}, {option: 'four', votes: 0}, {option: 'twelve', votes: 0}],
-    total: 0,
-    _id: 'suchunique2'
-  },
-  suchunique3: {
-    start: Date.now() - 27000,
-    time: 30000,
-    prompt: 'What is The Council?',
-    options: [{option: 'a game', votes: 0}, {option: 'nothing but truth', votes: 0}, {option: 'typical politics', votes: 0}],
-    total: 0,
-    _id: 'suchunique3'
-  },
-  suchunique4: {
-    start: Date.now() - 26000,
-    time: 30000,
-    prompt: 'Why is a raven like a writing desk?',
-    options: [{option: 'both have the letter e', votes: 0}, {option: 'it\'s not', votes: 0}, {option: 'why not', votes: 0}],
-    total: 0,
-    _id: 'suchunique4'
-  },
-  suchunique5: {
-    start: Date.now() - 25000,
-    time: 30000,
-    prompt: 'What should I do?',
-    options: [{option: 'ask for guidance', votes: 0}, {option: 'join the council', votes: 0}, {option: 'read about the council', votes: 0}],
-    total: 0,
-    _id: 'suchunique5'
-  }
-};
 
-module.exports = function(io) {
-  var report = function(question) {
-      console.log('Socket: reporting question');
-      if (open[question].total < 1) {
-        console.log('flipping coin');
-        var coinFlip = Math.floor(Math.random() * open[question].options.length);
-        console.log(coinFlip);
-        open[question].options[coinFlip].votes++;
-      }
+var sampleQuestions = [{
+  start: Date.now() - 29000,
+  time: 30000,
+  prompt: 'What is life?',
+  options: [{
+    option: 'a game',
+    votes: 0
+  }, {
+    option: 'a mystery',
+    votes: 0
+  }, {
+    option: 'the matrix',
+    votes: 0
+  }],
+  total: 0,
+  _id: 'suchunique'
+}, {
+  start: Date.now() - 28000,
+  time: 30000,
+  prompt: 'How many fingers am I holding up?',
+  options: [{
+    option: 'one',
+    votes: 0
+  }, {
+    option: 'four',
+    votes: 0}, {
+      option: 'twelve',
+      votes: 0
+    }],
+  total: 0,
+  _id: 'suchunique2'
+}, {
+  start: Date.now() - 27000,
+  time: 30000,
+  prompt: 'What is The Council?',
+  options: [{
+    option: 'a game',
+    votes: 0
+  }, {
+    option: 'nothing but truth',
+    votes: 0
+  }, {
+    option: 'typical politics', votes: 0
+  }],
+  total: 0,
+  _id: 'suchunique3'
+}, {
+  start: Date.now() - 26000,
+  time: 30000,
+  prompt: 'Why is a raven like a writing desk?',
+  options: [{
+    option: 'both have the letter e', votes: 0
+  }, {
+    option: 'it\'s not', votes: 0
+  }, {
+    option: 'why not', votes: 0
+  }],
+  total: 0,
+  _id: 'suchunique4'
+}, {
+  start: Date.now() - 25000,
+  time: 30000,
+  prompt: 'What should I do?',
+  options: [{
+    option: 'ask for guidance',
+    votes: 0
+  }, {
+    option: 'join the council',
+    votes: 0
+  }, {
+    option: 'read about the council',
+    votes: 0
+  }],
+  total: 0,
+  _id: 'suchunique5'
+}];
 
-      //reformat to result
-      open[question].options = open[question].options.sort(function(a, b) {
-        return b.votes - a.votes;
-      });
-      open[question].options[0].result = 'won';
-      for (var i = 1; i < open[question].options.length; i++) {
-        open[question].options[i].result = 'lost';
-      }
+module.exports = function(io, nano) {
 
-      io.to(question).emit('question:results', open[question]);
-      io.to('council').emit('question:ended', open[question]);
-      recent.unshift(open[question]);
-      recent.pop();
-      io.emit('update:recent', recent);
-      delete open[question];
-    };
+  nano.db.create('questions');
+  var questionStore = nano.db.use('questions');
 
-  var checkQuestions = function() {
-    if (open == {}) return;
-    for (var key in open) {
-      if ((Date.now() - open[key].start) > 30000) {
-        report(key);
-      }
-    }
-  };
-
-  setInterval(checkQuestions, 500);
-
-  var Socket = function(socket) {
+  function initSocket(socket) {
     userCount++;
-    io.to(socket.id).emit('recent', recent);
+
+    getQuestions(function(qs) {
+      io.to(socket.id).emit('recent', qs);
+    });
+
+    console.log('Socket: connect, Online ' + userCount);
     io.emit('online', userCount);
-    console.log('Socket: connect ' + userCount);
 
     socket.on('disconnect', function() {
-      console.log('Socket: disconnect');
       userCount--;
+      console.log('Socket: disconnect, Online ' + userCount);
       io.emit('online', userCount);
     });
 
     socket.on('question:submit', function(data) {
-      console.log('Socket: question submitted');
-      if (data.options.length < 1) return;
-      if (!data.prompt) data.prompt = '*Silence*...';
-      data.start = Date.now();
-      io.to('council').emit('question:new', data);
-      socket.join(data._id);
-      data.options.forEach(function(option) {
-        option.votes = 0;
+      console.log('Socket: question submitted ' + data.prompt);
+      pollQuestion(data, function(results) {
+        io.to(socket.id).emit('question:results', results);
       });
-      data.total = 0;
-      console.log('Adding: ' + JSON.stringify(data));
-      open[data._id] = data;
     });
 
     socket.on('load:questions', function(cb) {
       console.log('Socket: council member joined');
       socket.join('council');
       councilSize++;
-      cb(open);
+      cb(openQuestions);
     });
 
     socket.on('leave:council', function() {
@@ -128,13 +127,80 @@ module.exports = function(io) {
     });
 
     socket.on('vote', function(data) {
-      console.log('Socket: council member voted');
-      console.log(data);
-      open[data._id].total++;
-      open[data._id].options[data.index].votes++;
-      socket.join(data._id);
+      console.log('Socket: Vote Q:' + data._id + ' A:' + data.index);
+      vote(data);
     });
-  };
+  }
 
-  return Socket;
+  function pollQuestion(question) {
+    if (question.options.length < 1) return;
+
+    console.log('Question: ' + question.prompt);
+    openQuestions[question._id] = question;
+
+    question.created = Date.now();
+    setTimeout(function() {
+      report(question);
+    }, 30000);
+
+    io.to('council').emit('question:new', question);
+  }
+
+  function vote(v) {
+    openQuestions[v._id].total++;
+    openQuestions[v._id].options[v.index].votes++;
+  }
+
+  function report(question) {
+    console.log('Socket: reporting question');
+
+    if (question.total < 1) {
+      var coinFlip = Math.floor(Math.random() * question.options.length);
+      question.options[coinFlip].votes++;
+    }
+
+    //reformat to result
+    question.options = question.options.sort(function(a, b) {
+      return b.votes - a.votes;
+    });
+    _.first(question.options).result = 'won';
+    _.map(_.rest(question.options), function(o) {
+      o.result = 'lost';
+    });
+
+    questionStore.insert(question, function() {
+      getQuestions(function(qs) {
+        io.emit('update:recent', qs);
+      });
+    });
+    delete openQuestions[question._id];
+  }
+
+
+  function getQuestions(cb) {
+    var questions = [];
+    var complete = 0;
+
+    questionStore.list(function(err, body) {
+      if (err) return;
+
+      function checkComplete() {
+        if (complete === body.rows.length) {
+          console.log("ALL QUESTIONS", questions);
+          cb(questions);
+        }
+      }
+
+      _.map(body.rows, function(q) {
+        questionStore.get(q.id, function(err, body) {
+          if (err) return;
+          questions.push(body);
+          complete++;
+          checkComplete();
+        });
+      });
+    });
+  }
+
+  return initSocket;
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    nano = require('nano')('http://localhost:5984');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,8 +11,8 @@ app.use(express.static(__dirname + '/../public'));
 //socket io
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var socket = require('./routes/socket')(io);
-io.sockets.on('connection', socket);
+var socketHandler = require('./routes/socket')(io, nano);
+io.sockets.on('connection', socketHandler);
 
 server.listen(port, function() {
   console.log('Listening on ' + port);
